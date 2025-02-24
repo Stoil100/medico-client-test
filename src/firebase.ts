fddff179f007e0e6e7331a877f2183bd6153230a
Stoil100 <toli100ilov@gmail.com>
2025-02-24 22:34:02 +0200

    fix: Firebase config files

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -18,4 +19,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
